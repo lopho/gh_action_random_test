e716cbeb3262c40f911fc2550e0891c76d7f722f
lopho <lopho@users.noreply.github.com>
2022-11-26 16:56:18 +0100

    default generator

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import torch
 import os
 
-gen = torch.Generator()
-gen.manual_seed(0)
+torch.manual_seed(0)
 ts = []
 for i in range(1000):
-    a = torch.zeros((3,3))
-    a = a.random_(generator = gen)
+    a = torch.randn((3,3))
     ts.append(a)
 ts = torch.stack(ts)
 data_path = os.path.join('data', 'random.pt')
-if os.path.exists(data_path):
-    old = torch.load(data_path)
-    print('all\n', (old == ts).all())
+old_data = os.path.exists(data_path)
+old = torch.load(data_path) if old_data else None
 torch.save(ts, data_path)
+if old_data:
+    print('all\n', (old == ts).all())
+    assert (old == ts).all()
